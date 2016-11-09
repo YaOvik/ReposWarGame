cc5d56b6bf74a16732d53af6bb10c4424f34dbfa
Alexanderdenstore <backman300@gmail.com>
2016-11-09 11:01:39 +0100

    Nation Base Class

--- WarGame/WarGame/Classes/Nation.cs
+++ WarGame/WarGame/Classes/Nation.cs
@@ -12,6 +12,16 @@ namespace WarGame.Classes
         private string _nationName;
         private int _nationEndurance;
         private Color _nationColor;
+        private string _nationImagePath;
+
+
+        public Nation(string name, int endurance, Color color, string imagePath)
+        {
+            _nationName = name;
+            _nationEndurance = endurance;
+            _nationColor = color;
+            _nationImagePath = imagePath;
+        }
 
     }
 }
