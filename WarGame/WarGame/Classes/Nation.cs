c035e62cb93582440921478d030c05855c08ac96
Alexanderdenstore <backman300@gmail.com>
2016-11-09 11:25:05 +0100

    Revert "Add some stuff"
    This reverts commit c9cb93db283fa9adc0b6e2c18eb2dd6a97cf3976.

--- WarGame/WarGame/Classes/Nation.cs
+++ WarGame/WarGame/Classes/Nation.cs
@@ -9,21 +9,16 @@ namespace WarGame.Classes
 {
     class Nation
     {
-        public string _nationName;
-        public int _nationEndurance;
-        public int _nationXPosition;
-        public int _nationYPosition;
-
+        private string _nationName;
+        private int _nationEndurance;
         private Color _nationColor;
         private string _nationImagePath;
 
 
-        public Nation(string name, int endurance, int xpos, int ypos, Color color, string imagePath)
+        public Nation(string name, int endurance, Color color, string imagePath)
         {
             _nationName = name;
             _nationEndurance = endurance;
-            _nationXPosition = xpos;
-            _nationYPosition = ypos;
             _nationColor = color;
             _nationImagePath = imagePath;
         }
